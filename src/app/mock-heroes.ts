b915d867cb4d0f1ecc073aca398d8f5997bc5c12
Julio Ferro <julio@ampliato.com.br>
2018-09-19 15:14:34 -0300

    Basic project without services

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Hero 1'},
+    {id: 12, name: 'Hero 2'},
+    {id: 13, name: 'Hero 3'},
+    {id: 14, name: 'Hero 4'},
+    {id: 15, name: 'Hero 5'},
+    {id: 16, name: 'Hero 6'},
+    {id: 17, name: 'Hero 7'}
+];
